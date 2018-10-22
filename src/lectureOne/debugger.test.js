59f6f3ad4685d625696d5738cacf747f63df2527
plusgut <github@webentwickler2-0.de>
2018-10-22 19:23:38 +0200

    [type] changed to strings, for more confusion

--- src/lectureOne/debugger.test.js
+++ src/lectureOne/debugger.test.js
@@ -1,9 +1,15 @@
 describe("lectureOne", () => {
   it('multiplication by 0, always leads to zero', () => {
-    const result = multiply(0 , 1);
+    const result = addition('0' , '1');
     expect(result).toBe(0);
 
-    expect(multiply(0 , 2)).toBe(0);
-    expect(multiply(0 , -1)).toBe(0);
+    expect(addition('0' , '2')).toBe(0);
+    expect(addition('0' , '-1')).toBe(0);
+  });
+
+  xit('multiplication with not-number', () => {
+    expect(() => {
+      addition('foo');
+    }).toThrowException(new Error('Invalid parameter'))
   });
 });
